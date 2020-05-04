8fbc6e529b81cd6e417a8e83a34c721ebf6afdd7
Paweł Haładyj <pawel@haladyj.pl>
2020-05-04 09:00:02 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/springsecurity12/SpringSecurity12Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity12Application.class, args);
+	}
+
+}
---
+++ src/main/java/pl/haladyj/springsecurity12/config/AuthServerConfig.java
@@ -0,0 +1,32 @@
+package pl.haladyj.springsecurity12.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    public AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .scopes("read")
+                .authorizedGrantTypes("password");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
